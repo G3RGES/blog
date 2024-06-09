90b8fa38f71e16eea886bc450219b553c305f509
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-09 12:53:53 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import BlogHome from "./components/Blog/BlogHome";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <BlogHome />
+    </>
+  );
 }
 
 export default App;
