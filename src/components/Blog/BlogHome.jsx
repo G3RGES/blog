890b43b83f13cef5210f187d2872c73ad8dd9aa3
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-09 12:53:40 +0300

    Update BlogHome.jsx

--- src/components/Blog/BlogHome.jsx
+++ src/components/Blog/BlogHome.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
+const data = [
+  {
+    id: 1,
+    titleOne: "YOU CAN CHANGE THE WORLD",
+    titletwo: "STRONG STYLE",
+    paraOne: "How Women are Redirecting Hollywood",
+    paraTwo: "New Ways to Wear Denim Belt Bags make a Comeback",
+    name: "By Jasmin Smith",
+    subTitle: "How to Shop Mindfully",
+    qrCode: "/images/qrcode.png",
+    cover: "/Images/blog5.jpg",
+  },
+];
 const BlogHome = () => {
-  return <div>BlogHome</div>;
+  return (
+    <>
+      <h1 className="">Blog Home</h1>
+    </>
+  );
 };
 
 export default BlogHome;
