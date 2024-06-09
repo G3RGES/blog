ae0dd0b7980507823bd3b002f82f029f3e4e56ae
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-09 13:45:16 +0300

    Update SData.jsx

--- src/components/Slide/SData.jsx
+++ src/components/Slide/SData.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+const Sdata = [
+  {
+    id: 1,
+    cover: "/Images/slide-1.jpg",
+    category: "lifestyle",
+    title: "What are the best countries to visit?",
+    desc: "In this post, we are going to highlight the many benefits of blogging for business and how you can get started with",
+  },
+  {
+    id: 2,
+    cover: "/Images/slide-2.jpg",
+    category: "lifestyle",
+    title: "What are the best countries to visit?",
+    desc: "In this post, we are going to highlight the many benefits of blogging for business and how you can get started with",
+  },
+  {
+    id: 3,
+    cover: "/Images/slide-3.jpg",
+    category: "lifestyle",
+    title: "What are the best countries to visit?",
+    desc: "In this post, we are going to highlight the many benefits of blogging for business and how you can get started with",
+  },
+];
 
-const SData = () => {
-  return <div>SData</div>;
-};
-
-export default SData;
+export default Sdata;
