72ba9a60eb829a1ddfe14d67f7f8676918310726
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-09 14:55:47 +0300

    Update Slide.jsx

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import "./Style.css";
+import Sdata from "./SData";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Slide = () => {
-  return <div>Slide</div>;
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <>
+      <div className="slide">
+        <Slider {...settings}>
+          {Sdata.map((value) => {
+            return (
+              <div className="box">
+                <div className="img">
+                  <img src={value.cover} alt="" />
+                </div>
+                <div className="text">
+                  <span>{value.category}</span>
+                  <h2>{value.title}</h2>
+                  <p>{value.desc}</p>
+                </div>
+              </div>
+            );
+          })}
+        </Slider>
+      </div>
+    </>
+  );
 };
 
 export default Slide;
