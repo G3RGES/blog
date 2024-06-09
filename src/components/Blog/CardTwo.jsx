5b76f228de81236cd1ae9ead950f8be2e1be5924
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-09 14:55:39 +0300

    Update CardTwo.jsx

--- src/components/Blog/CardTwo.jsx
+++ src/components/Blog/CardTwo.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
+import Sdata from "./../Slide/SData";
+import "../Slide/Style.css";
+
 const CardTwo = () => {
-  return <div>CardTwo</div>;
+  return (
+    <>
+      <div className="CardTwo top slide">
+        {Sdata.map((value) => {
+          return (
+            <div className="box" key={value.id}>
+              <div className="img">
+                <img src={value.cover} alt="" />
+              </div>
+              <div className="text">
+                <span>{value.category}</span>
+                <h2>{value.title}</h2>
+                <p>{value.desc}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default CardTwo;
